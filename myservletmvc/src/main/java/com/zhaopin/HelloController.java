6f1447c727b75491e54e800324262a14b9290a27
gj <gj.w@zp.com>
2017-02-16 21:04:11 +0800

    foo bar

--- myservletmvc/src/main/java/com/zhaopin/HelloController.java
+++ myservletmvc/src/main/java/com/zhaopin/HelloController.java
@@ -1,9 +1,12 @@
 package com.zhaopin;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Date;
 
@@ -12,10 +15,15 @@ import java.util.Date;
  */
 @Controller
 @RequestMapping("/hello")
+@Scope("prototype")
 public class HelloController {
-    @RequestMapping(method = RequestMethod.GET,value = "test")
+    private int i;
+    @RequestMapping(method = RequestMethod.GET, value = "test")
     @ResponseBody
     public String test() {
-        return new Date().toString();
+        i ++;
+        javax.servlet.http.HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        return String.valueOf(i);
+        //return new Date().toString();
     }
 }
