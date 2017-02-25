b94201dc69f3d3c5867ae3bd2c85b49af8d6c5f5
gj <gj.w@zp.com>
2017-02-25 19:22:27 +0800

    vvvvvvv

---
+++ src/main/java/Employee.java
@@ -0,0 +1,28 @@
+/**
+ * foo...Created by wgj on 2017/2/20.
+ */
+public class Employee {
+    private int age;
+    protected String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
