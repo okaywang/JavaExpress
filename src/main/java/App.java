770ff7792871ca413aa2146d9d8ecb4f5f6a1189
okaywang <44@qq.com>
2017-02-27 14:52:11 +0800

    subtle change

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by wgj on 2017/1/21.
@@ -6,6 +9,11 @@ import java.io.IOException;
 public class App {
 
     public static void main(String... args) throws IOException {
+        List<String> s = Arrays.asList("aaa","bbbb");
+
+        ArrayList<String> bb;
+
+        System.out.println(s);
         System.out.println(Thread.activeCount());
     }
 }
--- thrift/src/main/java/com/zhaopin/service/JobServiceImpl.java
+++ thrift/src/main/java/com/zhaopin/service/JobServiceImpl.java
@@ -4,6 +4,11 @@ import com.zhaopin.thrift.JobDto;
 import com.zhaopin.thrift.JobService;
 import org.apache.thrift.TException;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Locale;
+
 /**
  * foo...Created by wgj on 2017/2/26.
  */
@@ -12,7 +17,7 @@ public class JobServiceImpl implements JobService.Iface {
     public JobDto getJob(int jobId) throws TException {
         JobDto dto = new JobDto();
         dto.setId(1200000);
-        dto.setJobName("地址勘探");
+        dto.setJobName("地址勘探" + LocalTime.now().toString());
         return dto;
     }
 }
